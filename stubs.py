a22431d98f192b70d97b4c1eb09002eee241d736
Michelle Steigerwalt <msteigerwalt@gmail.com>
2019-05-28 16:46:41 -0400

    Added admin badge to student list

--- stubs.py
+++ stubs.py
@@ -6,20 +6,23 @@ students =  [
 		'name':'Alice',
 		'completion': ';'.join(str(v) for v in random.sample(range(100), 5)),
 		'progress': random.randrange(50, 100),
-		'color': '#e809db'
+		'color': '#e809db',
+		'admin': False
 	},
 	{
 		'id':'2',
 		'name':'Bob',
 		'completion': ';'.join(str(v) for v in random.sample(range(100), 5)),
 		'progress': random.randrange(10, 50),
-		'color': '#0f7ff4'
+		'color': '#0f7ff4',
+		'admin': False
 	},
 	{
 		'id':'3',
 		'name':'Eve',
 		'completion': ';'.join(str(v) for v in random.sample(range(100), 5)),
 		'progress': random.randrange(50, 100),
-		'color': '#666'
+		'color': '#666',
+		'admin': True
 	}
 ];
