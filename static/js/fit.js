790bffceb7a3da0558c08f32892ad8f67c78dec2
Will Dayble <will@fitzroyacademy.com>
2019-06-27 14:41:15 +1000

    user slug js is now longer and weirder and hopefully more useful

--- static/js/fit.js
+++ static/js/fit.js
@@ -45,12 +45,34 @@ $( document ).ready(function() {
 
 
   // changing user URL
-  $("[data-fit-userslug_set]").on({
-    'change': function() {
-      $('[data-fit-userslug]').text('wefaewf');
-    }
-  });      
+
+  // slugification
+  function slugify(string) {
+  const a = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;'
+  const b = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string.toString().toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+  }
   
+  // change the actual url
+  $("[data-fit-userslug_set]").on({
+    'change, keyup': function() {
+      var slug = slugify($(this).val());
+      $('[data-fit-userslug]').text(slug);
+      $('[data-fit-userslug_secret]').val(slug);
+
+      // optionally mess with user input?
+      // $(this).val(slug);
+    }
+  });
 
 
   // ------------------------------------------------------------
