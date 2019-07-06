f0052fbb0a4a8d81aad705d3e7573da52461b644
Reid Savage <reid.savage@gmail.com>
2019-07-06 01:04:58 -0400

    add debug statements

--- scripts/assume-role.sh
+++ scripts/assume-role.sh
@@ -1,5 +1,7 @@
 sudo apt-get install -y jq awscli
 unset AWS_SESSION_TOKEN
+printenv
+env
 assumed_role=$(aws sts assume-role --role-arn "arn:aws:iam::593798515196:role/CircleCICrossAccountRole" --role-session-name "circleci-sandbox")
 export AWS_ACCESS_KEY_ID=$(echo $assumed_role | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $assumed_role | jq -r .Credentials.SecretAccessKey)
