5d3cec8cd3a41f9459e972acba3bcf0a19ae98b7
Reid Savage <reid.savage@gmail.com>
2019-07-06 01:35:52 -0400

    update circleci config to push to ECR

--- scripts/assume-role.sh
+++ scripts/assume-role.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y jq awscli
 unset AWS_SESSION_TOKEN
-assumed_role=$(aws sts assume-role --role-arn "arn:aws:iam::593798515196:role/CircleCICrossAccountRole" --role-session-name "circleci-sandbox")
+assumed_role=$(aws sts assume-role --role-arn "arn:aws:iam::$SANDBOX_ACCOUNT_ID:role/CircleCICrossAccountRole" --role-session-name "circleci-sandbox")
 export AWS_ACCESS_KEY_ID=$(echo $assumed_role | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $assumed_role | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $assumed_role | jq -r .Credentials.SessionToken)
