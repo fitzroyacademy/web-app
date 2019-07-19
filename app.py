050a458097bc264065be88df23551098cbbc678a
Michelle Steigerwalt <msteigerwalt@gmail.com>
2019-07-19 15:21:08 -0400

    Move partial objects into own blueprint (#58)
    Not ideal, but this is a backwards compatible refactor, so.

--- app.py
+++ app.py
@@ -14,12 +14,14 @@ import routes.user
 import routes.course
 import routes.lesson
 import routes.segment
+import routes.object
 
 app = Flask('FitzroyFrontend', static_url_path='')
 app.register_blueprint(routes.user.blueprint)
 app.register_blueprint(routes.course.blueprint, url_prefix="/course")
 app.register_blueprint(routes.lesson.blueprint, url_prefix="/course")
 app.register_blueprint(routes.segment.blueprint, url_prefix="/course")
+app.register_blueprint(routes.object.blueprint)
 
 def compile_sass():
     sass.compile(dirname=("static/assets/scss", 'static/css'))
@@ -121,102 +123,6 @@ def code():
 # --------------------------------------------------------------------------------
 # actually within the app now:
 
-@app.route('/course/<slug>')
-def course_preview(slug):
-    course = datamodels.get_course_by_slug(slug)
-    if course is None:
-        return redirect('/404')
-    return render_template('course_intro.html', course=course)
-
-@app.route('/course/<cid>/<lid>')
-def lesson_view(cid, lid):
-    lesson = datamodels.get_lesson_by_slug(cid, lid)
-    course = lesson.course
-    segment = lesson.segments[0]
-    data = {
-        'students': stubs.student_completion,
-        'active_lesson': lesson,    
-        'active_segment': segment,
-        'course': course
-    }
-    if course is None:
-        return redirect('/404')
-    return render_template('course.html', **data)
-
-@app.route('/course/<cid>/<lid>/<sid>')
-def lesson_segment(cid, lid=None, sid=None):
-	if lid is None:
-		course = datamodels.get_course_by_slug(cid)
-		lesson = course.lessons[0]
-		segment = lesson.segments[0]
-	else:
-		segment = datamodels.get_segment_by_slug(cid, lid, sid)
-		lesson = segment.lesson
-		course = lesson.course
-	data = {
-		'students': stubs.student_completion,
-		'active_lesson': lesson,	
-		'active_segment': segment,
-		'course': course
-	}
-	return render_template('course.html', **data)
-
-@app.route('/_segment/<sid>')
-def partial_segment(sid):
-	ext = None
-	if sid.endswith('.json'):
-		ext = "json"
-		sid = sid.split('.')[0]
-	active_segment = datamodels.get_segment(sid)
-	if active_segment is None:
-		raise "Segment not found: %s".format(sid)
-	data = {
-		'active_segment': active_segment
-	}
-	if ext is 'json':
-		dump = datamodels.dump(data['active_segment'])
-		data['active_segment'] = dump
-		return json.dumps(data)
-	return render_template('partials/_active_segment.html', **data)
-
-
-@app.route('/_lesson_resources/<lid>')
-def _lesson_resources(lid):
-	students =  [
-		{
-			'id':'1',
-			'name':'Alice',
-			'completion': ';'.join(str(v) for v in random.sample(range(100), 5)),
-			'progress': random.randrange(50, 100),
-			'color': '#e809db',
-			'admin': False
-		},
-		{
-			'id':'2',
-			'name':'Bob',
-			'completion': ';'.join(str(v) for v in random.sample(range(100), 5)),
-			'progress': random.randrange(10, 50),
-			'color': '#0f7ff4',
-			'admin': False
-		},
-		{
-			'id':'3',
-			'name':'Eve',
-			'completion': ';'.join(str(v) for v in random.sample(range(100), 5)),
-			'progress': random.randrange(50, 100),
-			'color': '#666',
-			'admin': True
-		}
-	]
-	active = None
-	lesson = datamodels.get_lesson(lid)
-	if lesson is None:
-		return {"error": "Lesson not found"}
-	data = {
-		'students': students,
-		'lesson': lesson
-	}
-	return render_template('partials/_lesson_resources.html', **data)
 
 @app.route('/lessons')
 def lessons():
