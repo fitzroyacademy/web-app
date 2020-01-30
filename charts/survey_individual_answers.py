10d32a679038530551452435c62aa8cca3dc8af1
mkarpicz <maciej.karpicz@gmail.com>
2020-01-30 23:42:45 +0100

    Custom css class for survey responses analytics view.

--- charts/survey_individual_answers.py
+++ charts/survey_individual_answers.py
@@ -7,6 +7,7 @@ def get_survey_individual_responses(survey):
     if survey.survey_type in [SurveyTypeEnum.plain_text, SurveyTypeEnum.emoji, SurveyTypeEnum.points_scale]:
         responses = SegmentSurveyResponse.objects().filter(SegmentSurveyResponse.survey == survey)
         course = survey.lesson.course
+        survey_id = survey.get_questions_template().get("survey_id", "")
         for response in responses:
             answer = response.get_response_dict()
             temp.append({
@@ -14,7 +15,9 @@ def get_survey_individual_responses(survey):
                 "user_fullname": "{} {}".format(response.user.first_name, response.user.last_name),
                 "avatar_url": response.user.profile_picture_url,
                 "is_teacher": response.user.teaches(course),
-                "chosen_answer": answer.get("chosen_answer", "").split("_")[-1]
+                "chosen_answer": answer.get("chosen_answer", "").split("_")[-1],
+                "custom_css_class": "{} {}".format(survey_id,
+                                                   answer.get("chosen_answer", ""))
             })
 
     return temp
