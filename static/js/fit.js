c3c51bc4641d2cf0f28fb3328f02468a29b5063d
Will Dayble <will@fitzroyacademy.com>
2020-04-30 13:12:26 +1000

    so many sales pages omg why dont you just redo the whole app will you fool

--- static/js/fit.js
+++ static/js/fit.js
@@ -15,6 +15,27 @@ $( document ).ready(function() {
   $('body').scrollspy({ target: '#scrollspy' })
 
 
+  // wistia preview thing
+  $('[data-fit-wistia-preview]').click(function(e) {
+    e.preventDefault();  
+    e.stopPropagation();
+    console.log($(this).data('fit-wistia-preview'));
+    $('[data-fit-wistia-preview-player]').empty().html('<script src="https://fast.wistia.com/embed/medias/' + $(this).data('fit-wistia-preview') + '.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_baba5nfzx9 videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/baba5nfzx9/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>');
+  });
+
+
+  $('[data-fit-fatquote] .dot').click(function(e) {
+    e.preventDefault();
+    
+    var target = $(this).attr('href');
+
+    $('[data-fit-fatquote] .dot').removeClass('active');
+    $('[data-fit-fatquote] .friend').removeClass('active');
+    
+    $(target).addClass('active');
+    $(this).addClass('active');
+  });
+  
 
 
   // standard tooltips
